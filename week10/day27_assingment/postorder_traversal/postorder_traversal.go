33d8f1212b6d549727e3a103a39b136e4fee76f8
Archit Agarwal <architagr@gmail.com>
2022-07-13 21:31:49 +0530

    week 10 day 27 assingment postorder Traversal

---
+++ week10/day27_assingment/postorder_traversal/postorder_traversal.go
@@ -0,0 +1,24 @@
+package postorder_traversal
+
+type treeNode struct {
+	left  *treeNode
+	value int
+	right *treeNode
+}
+
+func PreorderTraversal(A *treeNode) []int {
+
+	arr := make([]int, 0)
+	return traversal(A, arr)
+}
+
+func traversal(A *treeNode, arr []int) []int {
+	if A == nil {
+		return arr
+	}
+	
+	arr = traversal(A.left, arr)
+	arr = traversal(A.right, arr)
+	arr = append(arr, A.value)
+	return arr
+}
