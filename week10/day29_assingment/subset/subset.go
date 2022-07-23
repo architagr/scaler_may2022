0d83838f78aed8391c3a1b6a8c86662108364f77
Archit Agarwal <architagr@gmail.com>
2022-07-24 00:15:12 +0530

    week 10 day 29 assingment subset

--- week10/day29_assingment/subset/subset.go
+++ week10/day29_assingment/subset/subset.go
@@ -3,45 +3,46 @@ package subset
 import (
 	"sort"
 )
+
 func checkSet(n, k int) int {
 	return n & (1 << (k - 1))
 }
 
-func sortCustome(arr [][]int, p int, q int) bool{
-    a:=arr[p]
-    b:=arr[q]
+func sortCustome(arr [][]int, p int, q int) bool {
+	a := arr[p]
+	b := arr[q]
 
-     for  i := 0; i < len(a) && i < len(b); i++ {
-           
-            if (a[i] < b[i]) {
-                return true
-            }
+	for i := 0; i < len(a) && i < len(b); i++ {
 
-            if (a[i] > b[i]) {
-                return false
-            }
-      }
-       
-     if (len(a) > len(b)) {
-             return false
-       }
-     
-      return true
+		if a[i] < b[i] {
+			return true
+		}
+
+		if a[i] > b[i] {
+			return false
+		}
+	}
+
+	if len(a) > len(b) {
+		return false
+	}
+
+	return true
 
 }
-func subsets(A []int )  (arr [][]int) {
-    sort.Ints(A)
-    for i:=0;i< (1<<len(A)); i++{
-          a:=[]int{}
-         for j:=0; j< len(A) ; j++{
+func GetSubset(A []int) (arr [][]int) {
+	sort.Ints(A)
+	for i := 0; i < (1 << len(A)); i++ {
+		a := []int{}
+		for j := 0; j < len(A); j++ {
 
-             if checkSet(i,j+1)!=0{
-               a=append(a,A[j])
-             }
-         }
-         arr = append(arr, a)
-    } 
+			if checkSet(i, j+1) != 0 {
+				a = append(a, A[j])
+			}
+		}
+		arr = append(arr, a)
+	}
 
-    sort.Slice(arr, func(i, j int) bool {return sortCustome(arr,i,j)})
-    return 
+	sort.Slice(arr, func(i, j int) bool { return sortCustome(arr, i, j) })
+	return
 }
