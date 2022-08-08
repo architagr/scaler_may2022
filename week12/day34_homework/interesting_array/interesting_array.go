529acae524ed2f64025c3f40103d9afe788e51b3
Archit Agarwal <architagr@gmail.com>
2022-08-08 15:51:32 +0530

    week 12 day 35 homework Interesting Array

---
+++ week12/day34_homework/interesting_array/interesting_array.go
@@ -0,0 +1,15 @@
+package interesting_array
+
+func InterestingArray(A []int) string {
+	count := 0
+
+	for i := 0; i < len(A); i++ {
+		if A[i]%2 == 1 {
+			count++
+		}
+	}
+	if count%2 == 1 {
+		return "No"
+	}
+	return "Yes"
+}
