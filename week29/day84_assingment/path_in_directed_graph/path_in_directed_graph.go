14ba4ce0f6b6dea4aa9de7286ad692704f76a75e
Archit Agarwal <architagr@gmail.com>
2022-12-13 00:23:20 +0530

    week 29 day 84 assingment Path in Directed Graph

---
+++ week29/day84_assingment/path_in_directed_graph/path_in_directed_graph.go
@@ -0,0 +1,97 @@
+package path_in_directed_graph
+
+import "fmt"
+
+type Node struct {
+	Data int
+	next *Node
+}
+
+func NewNode(value int) *Node {
+	newNode := new(Node)
+	newNode.Data = value
+	return newNode
+}
+
+type Queue struct {
+	count      int
+	head, tail *Node
+}
+
+type IQueue interface {
+	Enqueue(data int)
+	Dequeue() *Node
+	IsEmpty() bool
+	Peep() (result int, err error)
+}
+
+func NewQueue() IQueue {
+	return new(Queue)
+}
+func (queue *Queue) Enqueue(data int) {
+	node := NewNode(data)
+	if queue.IsEmpty() {
+		queue.head = node
+		queue.tail = node
+	} else {
+		queue.tail.next = node
+		queue.tail = node
+	}
+	queue.count++
+}
+
+func (queue *Queue) Dequeue() *Node {
+	if queue.IsEmpty() {
+		return nil
+	}
+	node := queue.head
+	queue.head = queue.head.next
+	queue.count--
+	if queue.IsEmpty() {
+		queue.head = nil
+		queue.tail = nil
+	}
+	return node
+}
+func (queue *Queue) Peep() (result int, err error) {
+	if queue.IsEmpty() {
+		err = fmt.Errorf("no data")
+		return
+	}
+	result = queue.head.Data
+	return
+}
+
+func (queue *Queue) IsEmpty() bool {
+	return queue.count == 0
+}
+
+func PathDirectedGraph(A int, B [][]int) int {
+	visited := make([]bool, A+1)
+	queue := NewQueue()
+	at := make(map[int]map[int]bool)
+	for i := 0; i < len(B); i++ {
+		val, ok := at[B[i][0]]
+		if !ok {
+			val = make(map[int]bool)
+		}
+		val[B[i][1]] = true
+		at[B[i][0]] = val
+	}
+	queue.Enqueue(1)
+	visited[1] = true
+	for !queue.IsEmpty() {
+		x := queue.Dequeue().Data
+		val := at[x]
+		for key := range val {
+			if !visited[key] {
+				queue.Enqueue(key)
+				visited[key] = true
+			}
+		}
+	}
+	if visited[A] {
+		return 1
+	}
+	return 0
+}
